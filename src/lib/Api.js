9020b8670c0c90652cd423b4b9549ed59b668b38
coloro <colorolabs@gmail.com>
2018-06-05 13:33:52 -0400

    ajuste refund

--- src/lib/Api.js
+++ src/lib/Api.js
@@ -23,10 +23,7 @@ export function get_keystore(rut, token) {
 
 export function refund(address) {
   return send('/refund/' + address, {
-    method: 'GET',
-    headers: {
-      'Authorization': 'Bearer ' + session.data.token
-    }
+    method: 'GET'
   })
 }
 
